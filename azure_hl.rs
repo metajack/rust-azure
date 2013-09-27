fd4f383ec8c3eb63b50f1ca4d2202d2ca7bd0014
Jack Moffitt <jack@metajack.im>
2013-09-27 17:12:30 -0600

    Warning police; layers no longer a dependency.

--- azure_hl.rs
+++ azure_hl.rs
@@ -37,7 +37,6 @@ use geom::matrix2d::Matrix2D;
 use geom::point::Point2D;
 use geom::rect::Rect;
 use geom::size::Size2D;
-use layers::layers::TextureManager;
 use gl = opengles::gl2;
 use extra::arc::Arc;
 
